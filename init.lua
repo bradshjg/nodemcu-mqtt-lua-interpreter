f309c87e9df05c8aa60a6e4b49da6fe0db7e1240
Jimmy Bradshaw <jbradshaw@austin.utexas.edu>
2020-04-22 23:58:37 -0500

    put config in table

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
-mqtthost = "192.168.0.108"
-interpretername = "lua-interpreter"
-interpretertopic = "/" .. interpretername
+config = {
+    host="192.168.0.108",
+    clientid="lua-interpreter",
+    topic="/lua-interpreter"
+}
 
 function interpret(client, topic, data)
     print("message on " .. topic .. " with body " .. data)
-    if topic == interpretertopic and data ~= nil then
+    if topic == config["topic"] and data ~= nil then
         print("interpreting message")
         local req_t = sjson.decode(data)
         local status, val = pcall(loadstring(req_t["chunk"]))
@@ -13,8 +15,8 @@ function interpret(client, topic, data)
         end
         local replyto = req_t["reply-to"]
         if replyto ~= nil then
-            resp_t = {success=status, message=val}
-            resp_json = sjson.encode(resp_t)
+            local resp_t = {success=status, message=val}
+            local resp_json = sjson.encode(resp_t)
             print("publishing to " .. replyto .. " with " .. resp_json)
             client:publish(replyto, resp_json, 0, 0, function () print("publish success") end)
         end
@@ -22,13 +24,13 @@ function interpret(client, topic, data)
 end
 
 function onconnect(client)
-    client:subscribe(interpretertopic, 0, function () print("subscribe success") end)
+    client:subscribe(config["topic"], 0, function () print("subscribe success") end)
 end
 
 function mqttsetup()
-    local client = mqtt.Client(interpretername, 120)
+    local client = mqtt.Client(config["clientid"], 120)
     client:on("message", interpret)
-    client:connect(mqtthost, 1883, false, function ()
+    client:connect(config["host"], 1883, false, function ()
         print("connect success")
         onconnect(client)
     end,
@@ -50,7 +52,7 @@ function wificheck(timer)
 end
 
 function init()
-    inittimer = tmr.create()
+    local inittimer = tmr.create()
     inittimer:alarm(1000, tmr.ALARM_SEMI, wificheck)
 end
 
